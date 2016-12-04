207e5a1c0ca443ce111b6a018d2b39c24da40a8f
Michael <nikonof.m.i@gmail.com>
2016-12-04 04:41:50 +0300

    Implemented basic functionality

--- script/datalist.py
+++ script/datalist.py
@@ -7,10 +7,8 @@ data_list and wraps it's records.
 from itertools import zip_longest
 
 
-
 class TestData: 
-    def __init__(self, data_list_file, output_dir):
-        self.output_dir = output_dir
+    def __init__(self, data_list_file):
         self.experements = []
         self.parse_data_list(data_list_file)
     
@@ -25,29 +23,31 @@ class TestData:
 
     @staticmethod
     def experement_from(name, ref, gtf, r1, r2):
-        return Experement(name[len("# "):], ref[len("ref "):], gtf[len("grf "):],
-                        r1[len("r1 "):], r2[len("r2 "):])
+        return Experement(name[len("# "):], ref[len("ref: "):], 
+                            gtf[len("grf: "):], r1[len("r1: "):], r2[len("r2: "):])
 
     @staticmethod
     def check_note_structure(name, ref, gtf, r1, r2):
         if not name.startswith("# "):
             raise DataListExc("Expected test name: [# test_name]")
-        if not ref.startswith("ref "):
-            raise DataListExc("Expected reference path: [ref ref_path]")
-        if not gtf.startswith("gtf "):
-            raise DataListExc("Expected gtf path: [gtf gtf_path]")
-        if not r1.startswith("r1 "):
-            raise DataListExc("Expected path to file with left reads: [r1 reads1_path]")
-        if not r2.startswith("r2 "):
-            raise DataListExc("Expected path to file with right reads: [r2 reads2_path]")
+        if not ref.startswith("ref: "):
+            raise DataListExc("Expected reference path: [ref: ref_path]")
+        if not gtf.startswith("gtf: "):
+            raise DataListExc("Expected gtf path: [gtf: gtf_path]")
+        if not r1.startswith("r1: "):
+            raise DataListExc("Expected left reads in format [r1: file1&file2&file3]")
+        if not r2.startswith("r2: "):
+            raise DataListExc("Expected right reads in format [r2: file1&file2&file3]")
 
 class Experement:
     def __init__(self, name, ref, gtf, r1, r2):
         self.name = name
         self.ref = ref
         self.gtf = gtf
-        self.r1 = r1
-        self.r2 = r2
+        self.r1 = r1.split('&')
+        self.r2 = r2.split('&')
+        if len(self.r1) != len(self.r2):
+            raise DataListExc("Not the same number of left and right reads")
 
 class DataListExc(Exception):
     pass
